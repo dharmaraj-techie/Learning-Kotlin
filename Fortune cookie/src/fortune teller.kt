7405335f5086071369f5e7f04ccb941ee36e9212
Dharmaraj <dharmaraj.techie@gmail.com>
2020-04-04 21:17:01 +0530

    add getdate method

--- Fortune cookie/src/fortune teller.kt
+++ Fortune cookie/src/fortune teller.kt
@@ -1,9 +1,13 @@
 fun main(args : Array<String>) {
-    println("\nYour fortune is: ${getFortuneCookie()}")
-
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
+    }
 }
 
-fun getFortuneCookie() : String {
+fun getFortune(birthday : Int) : String {
     val fortunes = listOf( "You will have a great day!",
         "Things will go well for you today.",
         "Enjoy a wonderful day of success.",
@@ -11,7 +15,17 @@ fun getFortuneCookie() : String {
         "Today is a good day for exercising restraint.",
         "Take it easy and enjoy life!",
         "Treasure your friends, because they are your greatest fortune.")
+
+
+    val index = when (birthday) {
+        in 1..7 -> 4
+        28, 31 -> 2
+        else -> birthday.rem(fortunes.size)
+    }
+    return fortunes[index]
+}
+
+fun getBirthday(): Int {
     print("\nEnter your birthday: ")
-    val birthday = readLine()?.toIntOrNull() ?: 1
-    return fortunes[birthday.rem(fortunes.size)]
+    return readLine()?.toIntOrNull() ?: 1
 }
