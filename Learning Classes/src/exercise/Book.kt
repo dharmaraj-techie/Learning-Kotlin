1f4038631dc592f872eb8c563f55aff6a52d08ce
Dharmaraj <dharmaraj.techie@gmail.com>
2020-04-18 21:06:29 +0530

    learn pairs

--- Learning Classes/src/exercise/Book.kt
+++ Learning Classes/src/exercise/Book.kt
@@ -1,20 +1,18 @@
 package exercise
 
-open class Book(val title: String, val author: String, val year: String="nil") {
-     private var currentPage = 1
+open class Book(val title: String, val author: String, val year: String = "nil") {
+    private var currentPage = 1
 
-
-
-    open fun readPage(){
+    open fun readPage() {
         currentPage++
     }
 
     fun getTitleAndAuthor() = title to author
-    fun getAllDetails() = Triple(title,author,year)
-
+    fun getAllDetails() = Triple(title, author, year)
 
 
 }
+
 class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
 
     private var wordsRead = 0
