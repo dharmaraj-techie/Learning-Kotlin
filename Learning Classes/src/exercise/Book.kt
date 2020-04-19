b96ad867e2f70c7c6973466560086540db886776
Dharmaraj <dharmaraj.techie@gmail.com>
2020-04-19 08:28:04 +0530

    learn constants

--- Learning Classes/src/exercise/Book.kt
+++ Learning Classes/src/exercise/Book.kt
@@ -1,17 +1,34 @@
 package exercise
 
+    const val MAX_NUMBER_BOOKS = 5
+
 open class Book(val title: String, val author: String, val year: String = "nil") {
     private var currentPage = 1
+
+    companion object  {
+        const val BASE_URL = "http://www.turtlecare.net/"
+    }
+
+    var hasBooks = 0
 
     open fun readPage() {
         currentPage++
     }
 
     fun getTitleAndAuthor() = title to author
+
     fun getAllDetails() = Triple(title, author, year)
 
+    fun canBorrow() =  hasBooks < MAX_NUMBER_BOOKS
 
+    fun printUrl() {
+        //println(Constants.BASE_URL + title + ".html")
+    }
 }
+
+
+
+
 
 class eBook(title: String, author: String, var format: String = "text") : Book(title, author) {
 
@@ -19,5 +36,8 @@ class eBook(title: String, author: String, var format: String = "text") : Book(t
 
     override fun readPage() {
         wordsRead = wordsRead + 250
+    }
+    fun printDetails(){
+        println("Max num of books $MAX_NUMBER_BOOKS")
     }
 }
--- Learning Classes/src/main.kt
+++ Learning Classes/src/main.kt
@@ -1,11 +1,13 @@
 import Aquarium.Fish
 import exercise.Book
+import exercise.Book.Companion.BASE_URL
 
+import exercise.eBook
 
 
 fun main(args: Array<String>) {
 
-    val book = Book("india", "raj")
+    val book = eBook("india", "raj")
 
     val a = book.getTitleAndAuthor()
     val b = book.getAllDetails()
@@ -15,4 +17,8 @@ fun main(args: Array<String>) {
     println("${a.first} was written by ${a.second}")
     println("${b.first} was written by ${b.second} in the year ${b.third} ")
     println("$x was written by $y in the year $z ")
+
+    book.printDetails()
+
+
 }
